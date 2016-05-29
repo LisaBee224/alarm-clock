b96026d5de724782550292ba6c67fb636bf62d9f
LisaBee224 <lisakbuch@gmail.com>
2016-05-29 17:34:30 -0400

    Full alarm clock functionality

--- js.js
+++ js.js
@@ -10,8 +10,6 @@
     var aud = document.getElementById('file3');
   }
   else console.log('nothing checked');
-
-
     hour = document.getElementById('h').value;
     minute = document.getElementById('m').value;
     sec = document.getElementById('s').value;
@@ -20,11 +18,36 @@
     minute = minute * 60000;
     sec = sec * 1000;
    totalTime = hour+minute+sec;
-
    timer = setTimeout(function(){aud.play(); }, totalTime);
+
+
  };
   function cancel(){
     console.log("stopped");
     clearTimeout(timer);
 
+  };
+
+  function setalarm(){
+        if (document.getElementById('aud4').checked) {
+      var aud = document.getElementById('file1');
+  }else if(document.getElementById('aud5').checked) {
+    var aud = document.getElementById('file2');
+  }else if (document.getElementById('aud6').checked){
+    var aud = document.getElementById('file3');
+  }
+    d = new Date();
+    console.log(d);
+    currentH = d.getHours();
+    console.log(currentH);
+    currentM = d.getMinutes();
+    console.log(currentM);
+    alarmH = document.getElementById('hour').value;
+    alarmM = document.getElementById('minute').value;
+    destH = (alarmH - currentH) * 3600000;
+    destM = (alarmM - currentM) * 60000;
+    alarmTime = destH + destM;
+    console.log(alarmTime);
+
+    timer = setTimeout(function(){aud.play();}, alarmTime);
   };
